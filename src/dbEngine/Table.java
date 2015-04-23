98f2a3533dfaec76a81b11384b6bbe86623f4f3a
Ahmed Osama <ahmed.osama94@gmail.com>
2015-04-23 03:02:26 +0200

    just commented some code

--- src/dbEngine/Table.java
+++ src/dbEngine/Table.java
@@ -13,7 +13,8 @@ import exceptions.DBAppException;
 @SuppressWarnings("serial")
 public class Table implements Serializable {
 
-	static File x = new File("Metadata.csv");
+	private static File metadata = new File("Metadata.csv");
+
 	public Table(String strTableName,
 			Hashtable<String,String> htblColNameType,
 			Hashtable<String,String>htblColNameRefs,
@@ -21,66 +22,72 @@ public class Table implements Serializable {
 		writeMetadata(strTableName, htblColNameType, htblColNameRefs, strKeyColName);
 	}
 
+	// This method needs to be re-written
 	public void writeMetadata(String strTableName,
 			Hashtable<String,String> htblColNameType,
 			Hashtable<String,String>htblColNameRefs,
 			String strKeyColName) throws DBAppException{
 		try
 		{
-		    BufferedWriter writer = new BufferedWriter(new FileWriter(x,true));
-		    
-		    Enumeration<String> e = htblColNameType.keys();
+			BufferedWriter writer = new BufferedWriter(new FileWriter(metadata,true));
 
-		    Enumeration<String> d = htblColNameRefs.keys();
-		    while(e.hasMoreElements()){
-		    	// Table Name, Column Name, Column Type, Key, Indexed, References
-		  		String key = e.nextElement();
-		    	writer.append(strTableName + ",");
-		    	writer.append(key + ",");
-			    writer.append(htblColNameType.get(key) + ",");
-			    if(key==strKeyColName){
+			Enumeration<String> e = htblColNameType.keys();
 
-				    writer.append("True" + ",");
-				    writer.append("True" + ",");
-			    }
-			    else{
-			    	writer.append("False" + ",");
-			    	writer.append("False" + ",");
-			    }
-			    while(d.hasMoreElements()){
-			    	String key2 =d.nextElement();
-			    	if(key==key2){
-			    		writer.append(htblColNameRefs.get(key) + ",");
-			    	}
-			    }
-			    writer.append('\n');
-		    }
-		  	
-		    
-		    writer.flush();
-		    writer.close();
+			Enumeration<String> d = htblColNameRefs.keys();
+			while(e.hasMoreElements()){
+				// Table Name, Column Name, Column Type, Key, Indexed, References
+				String key = e.nextElement();
+				writer.append(strTableName + ",");
+				writer.append(key + ",");
+				writer.append(htblColNameType.get(key) + ",");
+				if(key==strKeyColName){
+
+					writer.append("True" + ",");
+					writer.append("True" + ",");
+				}
+				else{
+					writer.append("False" + ",");
+					writer.append("False" + ",");
+				}
+				while(d.hasMoreElements()){
+					String key2 =d.nextElement();
+					if(key==key2){
+						writer.append(htblColNameRefs.get(key) + ",");
+					}
+				}
+				writer.append('\n');
+			}
+
+
+			writer.flush();
+			writer.close();
 		}
 		catch(IOException e)
 		{
-		     e.printStackTrace();
+			e.printStackTrace();
 		}    
 
 	}
+
+	public static File getMetaData() {
+		return metadata;
+	}
+
 	public static void main(String [] args){
-		Hashtable<String, String> x = new Hashtable<String, String>();
-		x.put("name","string");
-		x.put("id", "int");
-		x.put("deptname","string");
-		Hashtable<String, String> y = new Hashtable<String, String>();
-		y.put("deptname","department");
-		try {
-			Table game =new Table("game2", x, y, "id");
-		} catch (DBAppException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+//		Hashtable<String, String> x = new Hashtable<String, String>();
+//		x.put("name","string");
+//		x.put("id", "int");
+//		x.put("deptname","string");
+//		Hashtable<String, String> y = new Hashtable<String, String>();
+//		y.put("deptname","department");
+//		try {
+//			Table game =new Table("game2", x, y, "id");
+//		} catch (DBAppException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
 	}
-	
-	}
+
+}
 
